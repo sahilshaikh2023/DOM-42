1c769b4a422d95b0ec5c08c059a80a392b7df104
sahilshaikh2023 <sahilshaikh6922@gmail.com>
2026-01-29 19:19:14 +0530

    dom project

--- app.js
+++ app.js
@@ -1,4 +1,52 @@
 const h1 = document.querySelector("h1");
+const btn = document.querySelector("button");
 
 
-h1.innerHTML ="changed";
+let ipl = [
+    {
+        name: "Chennai Super Kings",
+        bgColor: "#F7C700",
+        textBg: "#FFD700"
+    },
+    {
+        name: "Mumbai Indians",
+        bgColor: "#004BA0",
+        textBg: "#0078BC"
+    },
+    {
+        name: "Royal Challengers Bangalore",
+        bgColor: "#A51C30",
+        textBg: "#DA291C"
+    },
+    {
+        name: "Kolkata Knight Riders",
+        bgColor: "#2E0854",
+        textBg: "#3A225D"
+    },
+    {
+        name: "Rajasthan Royals",
+        bgColor: "#EA1A85",
+        textBg: "#FF69B4"
+    },
+    {
+        name: "Delhi Capitals",
+        bgColor: "#17479E",
+        textBg: "#2561AE"
+    }
+];
+
+btn.addEventListener("click", ()=>{
+
+
+    h1.innerHTML = "change";
+    let num = Math.floor(Math.random()*ipl.length);
+    let team = ipl[num];
+    h1.innerText = team.name ;
+
+    document.body.style.backgroundColor = team.bgColor;
+
+    // text background
+    h1.style.backgroundColor = team.textBg;
+
+
+})
